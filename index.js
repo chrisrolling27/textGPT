acf2a683cb379e3f8e85dc555879795d748aba4f
chris <chrisrolling27@gmail.com>
2023-04-07 10:49:40 -0700

    structure

--- index.js
+++ index.js
@@ -4,4 +4,62 @@ const twilio = require('twilio');
 const dotenv = require('dotenv');
 const bodyParser = require('body-parser');
 
+const app = express();
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {console.log(`Server running on port ${PORT}`);});
+
 dotenv.config();
+app.use(bodyParser.urlencoded({ extended: false }));
+
+const client = twilio(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
+const TWILIO_PHONE_NUMBER = process.env.TWILIO_PHONE_NUMBER;
+const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
+const GPT_API_ENDPOINT = 'https://api.openai.com/v1/engines/davinci-codex/completions';
+
+
+
+
+app.post('/sms', async (req, res) => {
+    const receivedMessage = req.body.Body;
+    const senderPhoneNumber = req.body.From;
+
+    const gptResponse = await sendToGPT(receivedMessage);
+
+    client.messages
+        .create({
+            body: gptResponse,
+            from: TWILIO_PHONE_NUMBER,
+            to: senderPhoneNumber
+        })
+        .then(() => {
+            res.status(200).send();
+        })
+        .catch((err) => {
+            console.error(err);
+            res.status(500).send();
+        });
+});
+
+async function sendToGPT(message) {
+    const prompt = `Texter says: ${message}\nAI Assistant:`;
+    const headers = {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${OPENAI_API_KEY}`
+    };
+    const data = {
+        prompt,
+        max_tokens: 150,
+        n: 1,
+        stop: null,
+        temperature: 0.8
+    };
+
+    try {
+        const response = await axios.post(GPT_API_ENDPOINT, data, { headers: headers });
+        return response.data.choices[0].text.trim();
+    } catch (error) {
+        console.error(error);
+        return 'Error processing your request.';
+    }
+}
+
